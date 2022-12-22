27c0a5ecb323c9b2aeee83d90957181ced6b16a1
Sean Yang <60932078+s303824@users.noreply.github.com>
2022-12-21 19:47:07 -0500

    Create tts.py

---
+++ server/python/tts.py
@@ -0,0 +1,13 @@
+import pyttsx3
+import sys
+
+# get the text to convert
+text = sys.argv[1]
+
+# create a text-to-speech engine
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')       #getting details of current voice
+engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male*
+
+engine.save_to_file(text, 'readaloud.mp3')
+engine.runAndWait()
