a9472327fd793992484e88471304f4120f0cb792
Sean Yang <60932078+s303824@users.noreply.github.com>
2022-12-22 15:56:36 -0500

    Update tts.py

--- server/python/tts.py
+++ server/python/tts.py
@@ -6,8 +6,6 @@ text = sys.argv[1]
 
 # create a text-to-speech engine
 engine = pyttsx3.init()
-voices = engine.getProperty('voices')       #getting details of current voice
-engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male*
 
 engine.save_to_file(text, './server/python/readaloud.mp3')
 engine.runAndWait()
