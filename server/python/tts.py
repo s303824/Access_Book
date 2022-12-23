6d3d97aa7de688120844fbe2e208b63318ed763d
Sean Yang <60932078+s303824@users.noreply.github.com>
2022-12-23 02:51:41 -0500

    reorganized files into two main folders

--- server/python/tts.py
+++ server/python/tts.py
@@ -7,5 +7,5 @@ text = sys.argv[1]
 # create a text-to-speech engine
 engine = pyttsx3.init()
 
-engine.save_to_file(text, './server/python/readaloud.mp3')
+engine.save_to_file(text, './python/readaloud.mp3')
 engine.runAndWait()
